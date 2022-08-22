50ac3c88d8a89912ad2d7a0ac57e97012286a8b5
wenner <julianwenner@gmail.com>
2022-08-22 20:01:29 -0300

    primer entrega

---
+++ javaScript/main.js
@@ -0,0 +1,11 @@
+let continuar=true;
+do{
+let respuesta=prompt("Tiene dientes y no come, tiene cabeza y no es hombre :ingrese respuesta de adivinanza");
+ 
+if(respuesta=="AJO"|| respuesta=="ajo"|| respuesta=="Ajo"){
+continuar=false;
+
+ }
+}while(continuar);
+
+console.log("VISCTORIA")
